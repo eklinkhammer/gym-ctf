2fbebde10142931b377023f968374c0f1c9ed4d4
Eric Klinkhammer <eklinkhammer@gmail.com>
2017-04-12 16:01:16 -0700

    Added skeleton envs with render.

--- gym_ctf/envs/ctf_env.py
+++ gym_ctf/envs/ctf_env.py
@@ -6,7 +6,10 @@ class CtfEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init(self):
-        pass
+        self.observation_space = None
+        self.action_space = None
+        self.reward_range = None
+        
 
     def _step(self,action):
         pass
@@ -15,5 +18,15 @@ class CtfEnv(gym.Env):
         pass
 
     def _render(self, mode='human', close=False):
-        pass
+        if close:
+            if self.viewer is not None:
+                self.viewer.close()
+                self.viewer = None
+
+        if self.viewer = None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(600, 400)
+
+        return self.viewer.render(return_rgb_array = mode == 'rgb_array')
     
+ 
---
+++ gym_ctf/envs/ctf_singleteam_env.py
@@ -0,0 +1,30 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+class CtfSingleTeamEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init(self):
+        self.observation_space = None
+        self.action_space = None
+        self.reward_range = None
+        
+
+    def _step(self,action):
+        pass
+
+    def _reset(self):
+        pass
+
+    def _render(self, mode='human', close=False):
+        if close:
+            if self.viewer is not None:
+                self.viewer.close()
+                self.viewer = None
+
+        if self.viewer = None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(600, 400)
+
+        return self.viewer.render(return_rgb_array = mode == 'rgb_array')
